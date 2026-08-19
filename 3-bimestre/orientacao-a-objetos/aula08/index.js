1270644822114eca1637341774c573155d35022f
Aristides <aristides.neto3@aluno.ce.gov.br>
2026-08-19 22:27:29 +0000

    feitos

--- 3-bimestre/orientacao-a-objetos/aula08/index.js
+++ 3-bimestre/orientacao-a-objetos/aula08/index.js
@@ -1,14 +1,19 @@
-class Cofre {
+class Cofre{
     #codigo
-    
+
     constructor(codigo){
         this.#codigo = codigo
     }
-    verificarCodigo(codigo) {
-        return codigo === this.#codigo
+
+    verificarCodigo(codigo){
+        if(codigo == this.#codigo){
+            return true
+        } else{
+            return false
+        }
     }
 }
 
-const cofre = new Cofre("1234")
+const senha = new Cofre(1234)
 
-module.exports = cofre
+module.exports = senha
