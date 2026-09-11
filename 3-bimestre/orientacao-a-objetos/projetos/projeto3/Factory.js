8a2c946f6aafeb6c007a039da062cf6905b89aa3
Aristides <208662144+Aristides66@users.noreply.github.com>
2026-09-11 18:48:26 +0000

    atualizar

--- 3-bimestre/orientacao-a-objetos/projetos/projeto3/Factory.js
+++ 3-bimestre/orientacao-a-objetos/projetos/projeto3/Factory.js
@@ -1,6 +1,31 @@
 const Notificacao = require("./Notificacao")
 // CRIE SUA SOLUÇÃO ABAIXO ================
 
+const {
+  NotificacaoEmail,
+  NotificacaoSMS,
+  NotificacaoApp
+} = require("./notificacao")
+
+class NotificacaoFactory {
+  static criar(tipo, mensagem) {
+    switch (tipo) {
+      case "email":
+        return new NotificacaoEmail(mensagem)
+
+      case "sms":
+        return new NotificacaoSMS(mensagem)
+
+      case "app":
+        return new NotificacaoApp(mensagem)
+
+      default:
+        throw new Error("Tipo de notificação inválido")
+    }
+  }
+}
+
+module.exports = NotificacaoFactory
 
 
 
--- 3-bimestre/orientacao-a-objetos/projetos/projeto3/Notificacao.js
+++ 3-bimestre/orientacao-a-objetos/projetos/projeto3/Notificacao.js
@@ -1,5 +1,39 @@
 // CRIE SUA SOLUÇÃO ABAIXO ================
 
+class Notificacao {
+  constructor(mensagem) {
+    this.mensagem = mensagem
+  }
+
+  enviar() {
+    throw new Error("O método enviar() deve ser implementado")
+  }
+}
+
+class NotificacaoEmail extends Notificacao {
+  enviar() {
+    return `E-mail enviado: ${this.mensagem}`
+  }
+}
+
+class NotificacaoSMS extends Notificacao {
+  enviar() {
+    return `SMS enviado: ${this.mensagem}`
+  }
+}
+
+class NotificacaoApp extends Notificacao {
+  enviar() {
+    return `Notificação no aplicativo: ${this.mensagem}`
+  }
+}
+
+module.exports = {
+  Notificacao,
+  NotificacaoEmail,
+  NotificacaoSMS,
+  NotificacaoApp
+}
 
 
 
